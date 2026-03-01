0fb4faf9f984e2ae76f739e33860b35f7352a69b
Brandon-Ashu <brandontolisso24@gmail.com>
2026-03-01 21:22:24 +0100

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,1 +1,18 @@
+/*
+========================================================================================================================
+Create Database and Schemas
+=========================================================================================================================
+Script purpose:
+    This script is meant to setup three schemas(Bronze,Silver,Gold) within the database which has already been created.
 
+  
+# Create 'Datawarehouse' Database
+  This project is being ran in PostgreSQL which is being ran in a docker container and accessed through DBvear GUI.
+  So the database has already been created.
+*/
+
+-- Create Datawarehouse schemas
+
+create schema Bronze;
+create schema Silver;
+create schema Gold;
